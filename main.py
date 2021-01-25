8c1ff33b9ec047b47e668921bebeaaa04d29fdb1
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-25 14:25:05 -0600

    did the paddle

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from turtle import Turtle, Screen
 from paddle import Paddle
 
 screen = Screen()
+screen.tracer(0)
 screen.listen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
 
 
+
 paddle = Paddle()
+
+
+
 
 
 screen.onkey(paddle.up, "Up")
@@ -16,7 +21,10 @@ screen.onkey(paddle.down, "Down")
 
 
 
+game_is_on = True 
 
+while game_is_on:
+    screen.update()
 
 
 
