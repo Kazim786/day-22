eb958fc581b2e656ea16c4bc4d6bf8db850f9cb3
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-25 15:56:38 -0600

    rearranging

--- main.py
+++ main.py
@@ -16,11 +16,9 @@ paddle_2 = Paddle((-350, 0))
 
 
 screen.onkey(paddle.up, "Up")
-screen.onkey(paddle_2.up, "W")
-
-
 screen.onkey(paddle.down, "Down")
-screen.onkey(paddle_2.down, "S")
+screen.onkey(paddle_2.up, "w")
+screen.onkey(paddle_2.down, "s")
 
 
 
