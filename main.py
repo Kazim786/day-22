367c405036ad3eec93cba7915020c65b62459dd9
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-02-06 21:21:40 -0600

    for some reason down isnt working

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ while game_is_on:
     if ball.ycor() > 300 or ball.ycor() < -300:
         ball.bounce()
 
-    # if ball.xcor() > 400 or ball.xcor() < -400:
-    #     screen.clear()
+    if ball.xcor() > 400 or ball.xcor() < -400:
+        ball.goto(0, 0)
 
     if ball.distance(paddle) < 50 and ball.xcor() > 320 or ball.distance(left_pad) < 50 and ball.xcor() > -320  :
         ball.padbounce()
