f59cdb802b61881f36f3b0deb9f5eec2792d032d
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-02-07 10:58:15 -0600

    score wont show

--- scoreboard.py
+++ scoreboard.py
@@ -6,16 +6,17 @@ class Scoreboard(Turtle):
     def init(self):
 
         super().__init__()
-
+        self.color("white")
         self.l_score = 0
         self.r_score = 0
-        self.color("white")
+        
         self.penup()
 
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        self.clear()
         self.goto(-100, 200)
         self.write(f"{self.l_score}", True, align= "center", font= ("Arial", 24, "normal"))
 
@@ -30,10 +31,12 @@ class Scoreboard(Turtle):
         
         # self.clear()
         self.l_score+=1
+        self.update_scoreboard()
         # self.write(f"Score: {self.l_score}", True, font= ("Arial", 24, "normal"))
 
     def right_point(self):
         
         # self.clear()
         self.r_score+=1
+        self.update_scoreboard()
         # self.write(f"Score: {self.r_score}", True, font= ("Arial", 24, "normal"))
